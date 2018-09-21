4218ee628aded705907987250ca5d85e41690f11
grokify <johncwang@gmail.com>
2018-09-21 03:43:48 -0700

    add buckets

---
+++ data/bucket_int.go
@@ -0,0 +1,36 @@
+package data
+
+func BucketMinMax(size, index int64) (int64, int64) {
+	max := (index + 1) * size
+	min := max - size + 1
+	return min, max
+}
+
+func BucketIndex(size, this int64) int64 {
+	if size == 0 {
+		panic("Bucket Size Cannot be Zero")
+	}
+	index := (this - 1) / size
+	return index
+}
+
+type Bucket struct {
+	Min int64
+	Max int64
+}
+
+func BucketsForMinMax(size, min, max int64) []Bucket {
+	if min > max {
+		tmp := min
+		min = max
+		max = tmp
+	}
+	minBucketIndex := BucketIndex(size, min)
+	maxBucketIndex := BucketIndex(size, max)
+	buckets := []Bucket{}
+	for this := minBucketIndex; this <= maxBucketIndex; this++ {
+		bucketMin, bucketMax := BucketMinMax(size, this)
+		buckets = append(buckets, Bucket{Min: bucketMin, Max: bucketMax})
+	}
+	return buckets
+}
---
+++ data/bucket_int_test.go
@@ -0,0 +1,40 @@
+package data
+
+import (
+	"testing"
+)
+
+var bucketTests = []struct {
+	size  int64
+	val   int64
+	index int64
+	min   int64
+	max   int64
+}{
+	{5, 1, 0, 1, 5},
+	{5, 2, 0, 1, 5},
+	{5, 3, 0, 1, 5},
+	{5, 4, 0, 1, 5},
+	{5, 5, 0, 1, 5},
+	{5, 6, 1, 6, 10},
+	{5, 7, 1, 6, 10},
+	{5, 8, 1, 6, 10},
+	{5, 9, 1, 6, 10},
+	{5, 10, 1, 6, 10},
+	{5, 11, 2, 11, 15}}
+
+func TestBucket(t *testing.T) {
+	for _, tt := range bucketTests {
+		min, max := BucketMinMax(tt.size, tt.index)
+		if min != tt.min || max != tt.max {
+			t.Errorf("data.BucketMinMax: with [%v,%v] want [%v,%v] got [%v,%v]",
+				tt.size, tt.index, tt.min, tt.max, min, max)
+		}
+
+		index := BucketIndex(tt.size, tt.val)
+		if index != tt.index {
+			t.Errorf("data.BucketIndex: with [%v,%v] want [%v] got [%v]",
+				tt.size, tt.val, tt.index, index)
+		}
+	}
+}
