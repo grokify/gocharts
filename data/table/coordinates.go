0080957ee25bd7f5e67b0bf090b4fffd3ef03cce
John Wang <johncwang@gmail.com>
2020-02-10 00:55:18 -0800

    covert x,y coordinates eto sheet location

---
+++ data/table/coordinates.go
@@ -0,0 +1,66 @@
+package table
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	ExcelMaxColCount = 16384
+	ExcelMaxRowCount = 1048576
+	Alphabet         = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ZZ               = uint32(702)
+)
+
+var alphabetSlice = strings.Split(Alphabet, "")
+
+func Quotient26ToPrefix(quotient uint32) string {
+	// " ABCDEF"
+	// "0123456"
+	// Max Quotient =26
+	if quotient > 27 {
+		panic(fmt.Sprintf("E_QUOTIENT26_OUT_OF_RANGE [0..26][%v]", quotient))
+	} else if quotient == 0 {
+		return ""
+	}
+	prefix := alphabetSlice[quotient-1] // A=1
+	return prefix
+}
+
+func Remainder26ToSuffix(remainder uint32) string {
+	if remainder == 0 {
+		return "Z"
+	}
+	if remainder > 25 {
+		panic(fmt.Sprintf("E_REMAINDER_OUT_OF_RANGE: [%v]", remainder))
+	}
+	letter := alphabetSlice[remainder-1]
+	return letter
+}
+
+func ColNumberToLetters(colNumber uint32) string {
+	if colNumber == 0 {
+		panic("Row cannot be zero. Row is 1 indexed.")
+	} else if colNumber > ZZ {
+		panic("Col greater than 702 is not currently supported")
+	}
+	quotient := int64(0)
+	if colNumber > 26 {
+		quotient = int64((colNumber - 1) / 26)
+	}
+	remainder := int64((colNumber) % 26)
+	if colNumber < 26 {
+		remainder = int64(colNumber)
+	}
+	prefix := Quotient26ToPrefix(uint32(quotient))
+	suffix := Remainder26ToSuffix(uint32(remainder))
+	return strings.TrimSpace(prefix) + strings.TrimSpace(suffix)
+}
+
+// CoordinatesToSheetLocation converts x, y integer coordinates
+// to a spreadsheet location such as "AA1" for col 27, row 1.
+func CoordinatesToSheetLocation(colNum, rowNum uint32) string {
+	colLet := ColNumberToLetters(colNum)
+	return colLet + strconv.Itoa(int(rowNum))
+}
---
+++ data/table/coordinates_test.go
@@ -0,0 +1,78 @@
+package table
+
+import (
+	"testing"
+)
+
+var quotient26ToPrefix = []struct {
+	quotient uint32
+	prefix   string
+}{
+	{0, ""},
+	{1, "A"},
+	{2, "B"},
+	{3, "C"}}
+
+func TestQuotient26ToPrefix(t *testing.T) {
+	for _, tt := range quotient26ToPrefix {
+		prefix := Quotient26ToPrefix(tt.quotient)
+		if prefix != tt.prefix {
+			t.Errorf("table.Quotient26ToPrefix: with [%v] want [%v] got [%v]",
+				tt.quotient, tt.prefix, prefix)
+		}
+	}
+}
+
+var colNumberToLetters = []struct {
+	colNumber     uint32
+	rowNumber     uint32
+	prefix        string
+	suffix        string
+	letters       string
+	sheetLocation string
+}{
+	{1, 1024, "", "A", "A", "A1024"},
+	{26, 1024, "", "Z", "Z", "Z1024"},
+	{27, 1024, "A", "A", "AA", "AA1024"},
+	{52, 1024, "A", "Z", "AZ", "AZ1024"},
+	{53, 1024, "B", "A", "BA", "BA1024"},
+	{78, 1024, "B", "Z", "BZ", "BZ1024"},
+	{79, 2048, "C", "A", "CA", "CA2048"},
+	{80, 2048, "C", "B", "CB", "CB2048"},
+	{676, 2048, "Y", "Z", "YZ", "YZ2048"},
+	{677, 2048, "Z", "Z", "ZA", "ZA2048"},
+	{702, 4096, "Z", "Z", "ZZ", "ZZ4096"}}
+
+type Instance struct {
+	ColNumber   uint32
+	Quotient26  uint32
+	Remainder26 uint32
+	PrefixWant  string
+	PrefixGot   string
+	SuffixWant  string
+	SuffixGot   string
+	LettersWant string
+	LettersGot  string
+}
+
+func TestColNumberToLetters(t *testing.T) {
+	for _, tt := range colNumberToLetters {
+		letters := ColNumberToLetters(tt.colNumber)
+		inst := Instance{
+			ColNumber:   tt.colNumber,
+			PrefixWant:  tt.prefix,
+			SuffixWant:  tt.suffix,
+			LettersWant: tt.letters,
+			LettersGot:  letters}
+		//fmtutil.PrintJSON(inst)
+		if inst.LettersGot != inst.LettersWant {
+			t.Errorf("table.ColNumberToLetters(%v) want [%v] got [%v]",
+				tt.colNumber, tt.letters, letters)
+		}
+		sheetLoc := CoordinatesToSheetLocation(tt.colNumber, tt.rowNumber)
+		if sheetLoc != tt.sheetLocation {
+			t.Errorf("table.CoordinatesToSheetLocation(%v,%v) want [%v] got [%v]",
+				tt.colNumber, tt.rowNumber, tt.sheetLocation, sheetLoc)
+		}
+	}
+}
