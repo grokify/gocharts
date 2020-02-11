1eaffe89697905053790622b123aab03fea3342a
John Wang <johncwang@gmail.com>
2020-02-11 03:45:27 -0800

    refactor

--- data/table/table.go
+++ data/table/table.go
@@ -1,7 +1,6 @@
 package table
 
 import (
-	"encoding/csv"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -10,7 +9,6 @@ import (
 	"strings"
 
 	//"github.com/grokify/gotilla/encoding/csvutil"
-	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/grokify/gotilla/encoding/jsonutil"
 )
 
@@ -204,56 +202,8 @@ func (t *TableData) WriteXLSX(path, sheetname string) error {
 	return WriteXLSX(path, sheetname, t)
 }
 
-func WriteXLSX(path, sheetname string, t *TableData) error {
-	f := excelize.NewFile()
-	// Create a new sheet.
-	index := f.NewSheet(sheetname)
-	// Set value of a cell.
-	rowBase := 0
-	if len(t.Columns) > 0 {
-		rowBase += 1
-		for i, cellValue := range t.Columns {
-			cellLocation := CoordinatesToSheetLocation(uint32(i), 0)
-			f.SetCellValue(sheetname, cellLocation, cellValue)
-		}
-	}
-	for y, row := range t.Records {
-		for x, cellValue := range row {
-			cellLocation := CoordinatesToSheetLocation(uint32(x), uint32(y+rowBase))
-			f.SetCellValue(sheetname, cellLocation, cellValue)
-		}
-	}
-	// Set active sheet of the workbook.
-	f.SetActiveSheet(index)
-	// Save xlsx file by the given path.
-	return f.SaveAs(path)
-}
-
 func (t *TableData) WriteCSV(path string) error {
 	return WriteCSV(path, t)
-}
-
-func WriteCSV(path string, t *TableData) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-
-	if len(t.Columns) > 0 {
-		err = writer.Write(t.Columns)
-		if err != nil {
-			return err
-		}
-	}
-	err = writer.WriteAll(t.Records)
-	if err != nil {
-		return err
-	}
-	writer.Flush()
-	return writer.Error()
 }
 
 func (t *TableData) RecordToMSS(record []string) map[string]string {
@@ -286,12 +236,4 @@ func (t *TableData) WriteJSON(path string, perm os.FileMode, jsonPrefix, jsonInd
 		return err
 	}
 	return ioutil.WriteFile(path, bytes, perm)
-}
-
-// WriteCSVSimple writes a file with cols and records data.
-func WriteCSVSimple(cols []string, records [][]string, filename string) error {
-	tbl := NewTableData()
-	tbl.Columns = cols
-	tbl.Records = records
-	return tbl.WriteCSV(filename)
 }
---
+++ data/table/write.go
@@ -0,0 +1,66 @@
+package table
+
+import (
+	"encoding/csv"
+	"os"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
+
+// WriteXLSX writes a table as an Excel XLSX file.
+func WriteXLSX(path, sheetname string, t *TableData) error {
+	f := excelize.NewFile()
+	// Create a new sheet.
+	index := f.NewSheet(sheetname)
+	// Set value of a cell.
+	rowBase := 0
+	if len(t.Columns) > 0 {
+		rowBase += 1
+		for i, cellValue := range t.Columns {
+			cellLocation := CoordinatesToSheetLocation(uint32(i), 0)
+			f.SetCellValue(sheetname, cellLocation, cellValue)
+		}
+	}
+	for y, row := range t.Records {
+		for x, cellValue := range row {
+			cellLocation := CoordinatesToSheetLocation(uint32(x), uint32(y+rowBase))
+			f.SetCellValue(sheetname, cellLocation, cellValue)
+		}
+	}
+	// Set active sheet of the workbook.
+	f.SetActiveSheet(index)
+	// Save xlsx file by the given path.
+	return f.SaveAs(path)
+}
+
+// WriteCSV writes the table as a CSV>
+func WriteCSV(path string, t *TableData) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	if len(t.Columns) > 0 {
+		err = writer.Write(t.Columns)
+		if err != nil {
+			return err
+		}
+	}
+	err = writer.WriteAll(t.Records)
+	if err != nil {
+		return err
+	}
+	writer.Flush()
+	return writer.Error()
+}
+
+// WriteCSVSimple writes a file with cols and records data.
+func WriteCSVSimple(cols []string, records [][]string, filename string) error {
+	tbl := NewTableData()
+	tbl.Columns = cols
+	tbl.Records = records
+	return tbl.WriteCSV(filename)
+}
