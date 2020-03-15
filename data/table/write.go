da610582e9c406788837aa3bc9fb665fc85746d9
John Wang <johncwang@gmail.com>
2020-03-15 01:55:00 -0700

    add ability to write multiple XLSX sheets

--- data/table/write.go
+++ data/table/write.go
@@ -2,6 +2,7 @@ package table
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
@@ -39,28 +40,43 @@ func WriteCSVSimple(cols []string, records [][]string, filename string) error {
 	return tbl.WriteCSV(filename)
 }
 
+/*
 // WriteXLSX writes a table as an Excel XLSX file.
 func WriteXLSX(path, sheetname string, t *TableData) error {
+	t.Name = sheetname
+	return WriteXLSXMore(path, []*TableData{t})
+}*/
+
+// WriteXLSX writes a table as an Excel XLSX file.
+func WriteXLSX(path string, tbls []*TableData) error {
+	fmt.Println(len(tbls))
+	//panic("A")
 	f := excelize.NewFile()
 	// Create a new sheet.
-	index := f.NewSheet(sheetname)
-	// Set value of a cell.
-	rowBase := 0
-	if len(t.Columns) > 0 {
-		rowBase++
-		for i, cellValue := range t.Columns {
-			cellLocation := CoordinatesToSheetLocation(uint32(i), 0)
-			f.SetCellValue(sheetname, cellLocation, cellValue)
+	for i, t := range tbls {
+		fmt.Printf("[%v][%v]\n", i, t.Name)
+		sheetname := t.Name
+		index := f.NewSheet(sheetname)
+		// Set value of a cell.
+		rowBase := 0
+		if len(t.Columns) > 0 {
+			rowBase++
+			for i, cellValue := range t.Columns {
+				cellLocation := CoordinatesToSheetLocation(uint32(i), 0)
+				f.SetCellValue(sheetname, cellLocation, cellValue)
+			}
+		}
+		for y, row := range t.Records {
+			for x, cellValue := range row {
+				cellLocation := CoordinatesToSheetLocation(uint32(x), uint32(y+rowBase))
+				f.SetCellValue(sheetname, cellLocation, cellValue)
+			}
+		}
+		// Set active sheet of the workbook.
+		if i == 0 {
+			f.SetActiveSheet(index)
 		}
 	}
-	for y, row := range t.Records {
-		for x, cellValue := range row {
-			cellLocation := CoordinatesToSheetLocation(uint32(x), uint32(y+rowBase))
-			f.SetCellValue(sheetname, cellLocation, cellValue)
-		}
-	}
-	// Set active sheet of the workbook.
-	f.SetActiveSheet(index)
 	// Save xlsx file by the given path.
 	return f.SaveAs(path)
 }
