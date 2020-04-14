611ecd320b139991bd7310c2665b6e048fea59b0
John Wang <johncwang@gmail.com>
2020-04-14 02:22:44 -0700

    add roadmap times

---
+++ data/roadmap/time.go
@@ -0,0 +1,18 @@
+package roadmap
+
+import (
+	"github.com/grokify/gotilla/math/mathutil"
+	"github.com/grokify/gotilla/time/timeutil"
+)
+
+// QuarterInt32sBeginEnd converts relative and
+// default quarter times to absolute int32
+// quarter numbers.
+func QuarterInt32sBeginEnd(begin, end int32) (int32, int32) {
+	if begin == 0 && end == 0 {
+		begin = -1
+		end = 4
+	}
+	return mathutil.MinMaxInt32(
+		timeutil.QuartersInt32RelToAbs(begin, end))
+}
