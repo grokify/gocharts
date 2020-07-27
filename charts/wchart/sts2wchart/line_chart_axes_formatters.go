3aff735d15a3222f13fc946daec4856642aac47e
John Wang <johncwang@gmail.com>
2020-07-27 03:49:58 -0700

    feat: add additional sts2wchart Y-axis formatters

---
+++ charts/wchart/sts2wchart/line_chart_axes_formatters.go
@@ -0,0 +1,33 @@
+package sts2wchart
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/grokify/gotilla/strconv/strconvutil"
+)
+
+func YAxisTickFormatSimple(raw float64) string {
+	return strconvutil.Int64Abbreviation(int64(raw))
+}
+
+func YAxisTickFormatPercent(raw float64) string {
+	return fmt.Sprintf("%.1f%%", raw*100)
+}
+
+func YAxisTickFormatDollars(raw float64) string {
+	abbr := strconvutil.Int64Abbreviation(int64(raw))
+	return "$" + abbr
+}
+
+var rxMrr = regexp.MustCompile(`(?i)\bmrr\b`)
+
+func FormatYTickFunc(seriesName string) func(float64) string {
+	return func(val float64) string {
+		abbr := strconvutil.Int64Abbreviation(int64(val))
+		if rxMrr.MatchString(seriesName) {
+			return "$" + abbr
+		}
+		return abbr
+	}
+}
