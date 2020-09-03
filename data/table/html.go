8b42d0f04f86ab22477c09ff4310619b35e669d1
John Wang <johncwang@gmail.com>
2020-09-02 23:00:47 -0700

    feat: table: add ToHTML()

---
+++ data/table/html.go
@@ -0,0 +1,54 @@
+package table
+
+import (
+	"fmt"
+	"html"
+	"strings"
+)
+
+// ToHTML converts `*TableData` to HTML.
+func ToHTML(tbl *TableData, domID string, escapeHTML bool) string {
+	tHTML := "<table>"
+	domID = strings.TrimSpace(domID)
+	if len(domID) > 0 {
+		tHTML = fmt.Sprintf("<table id=\"%s\">", domID)
+	}
+	if len(tbl.Columns) > 0 {
+		if escapeHTML {
+			cols := []string{}
+			for _, col := range tbl.Columns {
+				cols = append(cols, html.EscapeString(col))
+			}
+			tHTML += "<thead><tr><th>" + strings.Join(cols, "</th><th>") + "</th></tr></thead>"
+		} else {
+			tHTML += "<thead><tr><th>" + strings.Join(tbl.Columns, "</th><th>") + "</th></tr></thead>"
+		}
+	}
+	if len(tbl.Records) > 0 {
+		tHTML += "<tbody>"
+		fmtFunc := tbl.FormatterFunc()
+		for _, row := range tbl.Records {
+			tHTML += "<tr>"
+			for x, cell := range row {
+				cfmt, err := fmtFunc(cell, uint(x))
+				if err != nil {
+					if escapeHTML {
+						tHTML += "<td>" + html.EscapeString(cell) + "</td>"
+					} else {
+						tHTML += "<td>" + cell + "</td>"
+					}
+				} else {
+					if escapeHTML {
+						tHTML += "<td>" + html.EscapeString(cfmt.(string)) + "</td>"
+					} else {
+						tHTML += "<td>" + cfmt.(string) + "</td>"
+					}
+				}
+			}
+			tHTML += "</tr>"
+		}
+		tHTML += "</tbody>"
+	}
+	tHTML += "</table>"
+	return tHTML
+}
