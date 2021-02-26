7cdd7f71cbf77584c87a94e479d69e7dbe6ad006
John Wang <johncwang@gmail.com>
2021-02-25 18:19:39 -0800

    cleanup: table: remove unneeded code

--- data/table/write.go
+++ data/table/write.go
@@ -104,101 +104,6 @@ func FormatTimeAndFloats(val string, colIdx uint) (interface{}, error) {
 	return num, nil
 }
 
-/*
-type TableFormatter struct {
-	Table     *TableData
-	FormatMap map[int]string
-	Formatter func(val string, colIdx uint) (interface{}, error)
-}
-
-func (tf *TableFormatter) FormatterFunc() func(val string, colIdx uint) (interface{}, error) {
-	if tf.FormatMap == nil || len(tf.FormatMap) == 0 {
-		if tf.Formatter == nil {
-			return FormatStrings
-		}
-		return tf.Formatter
-	}
-	return func(val string, colIdx uint) (interface{}, error) {
-		if fmtType, ok := tf.FormatMap[int(colIdx)]; ok {
-			fmtType := strings.ToLower(strings.TrimSpace(fmtType))
-			if len(fmtType) > 0 {
-				switch fmtType {
-				case "float":
-					floatVal, err := strconv.ParseFloat(val, 64)
-					if err != nil {
-						return val, err
-					}
-					return floatVal, nil
-				case "int":
-					intVal, err := strconv.Atoi(val)
-					if err != nil {
-						return val, err
-					}
-					return intVal, nil
-				case "time":
-					dtVal, err := time.Parse(time.RFC3339, val)
-					if err != nil {
-						return val, err
-					} else {
-						return dtVal, nil
-					}
-				}
-			}
-		}
-		return val, nil
-	}
-}
-
-// WriteXLSXFormatted writes a table as an Excel XLSX file with
-// row formatter option.
-func WriteXLSXFormatted(path string, tbls ...*TableFormatter) error {
-	f := excelize.NewFile()
-	// Delete default sheet.
-	f.DeleteSheet(f.GetSheetName(f.GetSheetIndex("Sheet1")))
-	f.DeleteSheet("Sheet1")
-	// Create a new sheet.
-	sheetNum := 0
-	for i, tf := range tbls {
-		if tf == nil || tf.Table == nil {
-			continue
-		}
-		t := tf.Table
-		sheetNum++
-		sheetname := strings.TrimSpace(t.Name)
-		if len(sheetname) == 0 {
-			sheetname = fmt.Sprintf("Sheet%d", sheetNum)
-		}
-		index := f.NewSheet(sheetname)
-		// Set value of a cell.
-		rowBase := 0
-		if len(t.Columns) > 0 {
-			rowBase++
-			for i, cellValue := range t.Columns {
-				cellLocation := CoordinatesToSheetLocation(uint32(i), 0)
-				f.SetCellValue(sheetname, cellLocation, cellValue)
-			}
-		}
-		fmtFunc := tf.FormatterFunc()
-		for y, row := range t.Records {
-			for x, cellValue := range row {
-				cellLocation := CoordinatesToSheetLocation(uint32(x), uint32(y+rowBase))
-				formattedVal, err := fmtFunc(cellValue, uint(x))
-				if err != nil {
-					return errors.Wrap(err, "gocharts/data/tables/write.go/WriteXLSXFormatted.Error.FormatCellValue")
-				}
-				f.SetCellValue(sheetname, cellLocation, formattedVal)
-			}
-		}
-		// Set active sheet of the workbook.
-		if i == 0 {
-			f.SetActiveSheet(index)
-		}
-	}
-	// Save xlsx file by the given path.
-	return f.SaveAs(path)
-}
-*/
-
 func (tbl *Table) FormatterFunc() func(val string, colIdx uint) (interface{}, error) {
 	if tbl.FormatMap == nil || len(tbl.FormatMap) == 0 {
 		if tbl.FormatFunc == nil {
@@ -254,7 +159,6 @@ func WriteXLSX(path string, tables ...*Table) error {
 		if tbl == nil {
 			continue
 		}
-		//t := tf.Table
 		sheetNum++
 		sheetname := strings.TrimSpace(tbl.Name)
 		if len(sheetname) == 0 {
