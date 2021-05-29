9659d42167d01434773b328695964e5da29e3187
John Wang <johncwang@gmail.com>
2021-05-29 12:44:28 -0700

    feat: data/table: add `Columns.RowMap()`

--- data/table/column.go
+++ data/table/column.go
@@ -1,7 +1,13 @@
 package table
 
+import (
+	"strings"
+)
+
+// Columns represents a slice of string with tabular functions.
 type Columns []string
 
+// RowVal returns a single row value.
 func (cols Columns) RowVal(colName string, row []string) string {
 	for colIdx, colNameTry := range cols {
 		if colNameTry == colName {
@@ -13,10 +19,27 @@ func (cols Columns) RowVal(colName string, row []string) string {
 	return ""
 }
 
+// RowVals returns a slice of values.
 func (cols Columns) RowVals(colNames []string, row []string) []string {
 	vals := []string{}
 	for _, colName := range colNames {
 		vals = append(vals, cols.RowVal(colName, row))
 	}
 	return vals
+}
+
+// RowMap converts a CSV row to a `map[string]string`.
+func (cols Columns) RowMap(row []string, omitEmpty bool) map[string]string {
+	mss := map[string]string{}
+	for i, key := range cols {
+		if i < len(row) {
+			val := strings.TrimSpace(row[i])
+			if !omitEmpty || len(val) > 0 {
+				mss[key] = row[i]
+			}
+		} else if !omitEmpty {
+			mss[key] = ""
+		}
+	}
+	return mss
 }
