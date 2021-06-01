f541c153a5287b15038a3d8bf67be7abe0dad584
John Wang <johncwang@gmail.com>
2021-06-01 02:13:52 -0700

    feat: data/table: add `Columns.RowValFloat64()`, `Columns.RowValInt()`, `Columns.RowValTime()`

--- data/table/column.go
+++ data/table/column.go
@@ -1,29 +1,68 @@
 package table
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 // Columns represents a slice of string with tabular functions.
 type Columns []string
 
+// MustRowVal returns a single row value.
+func (cols Columns) MustRowVal(colName string, row []string) string {
+	val, err := cols.RowVal(colName, row)
+	if err != nil {
+		return ""
+	}
+	return val
+}
+
 // RowVal returns a single row value.
-func (cols Columns) RowVal(colName string, row []string) string {
+func (cols Columns) RowVal(colName string, row []string) (string, error) {
 	for colIdx, colNameTry := range cols {
 		if colNameTry == colName {
 			if colIdx < len(row) {
-				return row[colIdx]
+				return row[colIdx], nil
 			}
 		}
 	}
-	return ""
+	return "", fmt.Errorf("columnName [%s] not found", colName)
 }
 
-// RowVals returns a slice of values.
-func (cols Columns) RowVals(colNames []string, row []string) []string {
+// RowValFloat64 returns a single row value.
+func (cols Columns) RowValFloat64(colName string, row []string) (float64, error) {
+	val, err := cols.RowVal(colName, row)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseFloat(val, 64)
+}
+
+// RowValInt returns a single row value.
+func (cols Columns) RowValInt(colName string, row []string) (int, error) {
+	val, err := cols.RowVal(colName, row)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(val)
+}
+
+// RowValTime returns a single row value.
+func (cols Columns) RowValTime(colName string, row []string) (time.Time, error) {
+	val, err := cols.RowVal(colName, row)
+	if err != nil {
+		return time.Now(), err
+	}
+	return time.Parse(time.RFC3339, val)
+}
+
+// MustRowVals returns a slice of values.
+func (cols Columns) MustRowVals(colNames []string, row []string) []string {
 	vals := []string{}
 	for _, colName := range colNames {
-		vals = append(vals, cols.RowVal(colName, row))
+		vals = append(vals, cols.MustRowVal(colName, row))
 	}
 	return vals
 }
