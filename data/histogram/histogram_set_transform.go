0ff598c3bddaa01ed1f5ad9df83f0afe78758c1a
John Wang <johncwang@gmail.com>
2021-06-01 13:17:05 -0700

    feat: data/histogram: `HistogramSet.TransformHistogramNames()`, `HistogramSet.TransformHistogramNamesExactMatch()`, `HistogramSet.TransformHistogramNamesByPrefix()`

---
+++ data/histogram/histogram_set_transform.go
@@ -0,0 +1,56 @@
+package histogram
+
+import (
+	"strings"
+)
+
+// TransformHistogramNames modifies histogram names and returns a new
+// histogram set.
+func (hset *HistogramSet) TransformHistogramNames(xfFunc func(input string) string) *HistogramSet {
+	if hset == nil {
+		return nil
+	}
+	newHset := NewHistogramSet(hset.Name)
+	for histName, hist := range hset.HistogramMap {
+		for binName, binCount := range hist.Bins {
+			newHset.Add(xfFunc(histName), binName, binCount)
+		}
+	}
+	return newHset
+}
+
+// TransformHistogramNamesExactMatch modifies bin names and returns a new
+// histogram.
+func (hset *HistogramSet) TransformHistogramNamesExactMatch(xfMap map[string]string) *HistogramSet {
+	if hset == nil {
+		return nil
+	}
+	return hset.TransformHistogramNames(
+		func(oldName string) string {
+			for oldNameTry, newName := range xfMap {
+				if oldNameTry == oldName {
+					return newName
+				}
+			}
+			return oldName
+		},
+	)
+}
+
+// TransformHistogramNamesByPrefix modifies bin names and returns a new
+// histogram.
+func (hset *HistogramSet) TransformHistogramNamesByPrefix(xfMap map[string]string) *HistogramSet {
+	if hset == nil {
+		return nil
+	}
+	return hset.TransformHistogramNames(
+		func(oldName string) string {
+			for oldPrefix, newName := range xfMap {
+				if strings.Index(oldName, oldPrefix) == 0 {
+					return newName
+				}
+			}
+			return oldName
+		},
+	)
+}
