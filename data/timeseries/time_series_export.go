274dcf44d0971fc6cb80bac396d0174b2f862ec1
grokify <johncwang@gmail.com>
2022-04-04 12:33:52 -0700

    feat: data/timeseries: add `TimeSeries.TableMonthXOX()`

--- data/timeseries/time_series_export.go
+++ data/timeseries/time_series_export.go
@@ -24,8 +24,8 @@ func TimeFormatNiceQuarter(dt time.Time) string {
 	return timeutil.FormatQuarterYYQ(dt)
 }
 
-// ToTable generates a `table.Table` given a `TimeSeries`.
-func (ts *TimeSeries) ToTable(tableName, dateColumnName, countColumnName string, dtFmt func(dt time.Time) string) table.Table {
+// Table generates a `table.Table` given a `TimeSeries`.
+func (ts *TimeSeries) Table(tableName, dateColumnName, countColumnName string, dtFmt func(dt time.Time) string) table.Table {
 	// previously only took dateColumnName as a parameter.
 	if len(strings.TrimSpace(dateColumnName)) == 0 {
 		switch ts.Interval {
@@ -68,6 +68,61 @@ func (ts *TimeSeries) ToTable(tableName, dateColumnName, countColumnName string,
 	return tbl
 }
 
+func (ts *TimeSeries) TableMonthXOX(timeFmtColName, seriesName, valuesName, yoyName, qoqName string) table.Table {
+	if len(seriesName) == 0 {
+		seriesName = "Series"
+	}
+	if len(valuesName) == 0 {
+		valuesName = "Values"
+	}
+	if len(yoyName) == 0 {
+		yoyName = "YoY"
+	}
+	if len(qoqName) == 0 {
+		qoqName = "QoQ"
+	}
+
+	tsm := ts.ToMonth(true)
+	tbl := table.NewTable("")
+	cols := []string{seriesName}
+	times := tsm.TimeSlice(true)
+	for _, dt := range times {
+		cols = append(cols, dt.Format(timeFmtColName))
+	}
+	tbl.Columns = cols
+	tbl.FormatMap = map[int]string{
+		-1: table.FormatFloat,
+		0:  table.FormatString}
+
+	yoy := tsm.TimeSeriesMonthYOY()
+	qoq := tsm.TimeSeriesMonthQOQ()
+
+	valData := []string{valuesName}
+	yoyData := []string{yoyName}
+	qoqData := []string{qoqName}
+	for _, dt := range times {
+		tiVal, err := tsm.Get(dt)
+		if err != nil {
+			panic("internal time not found")
+		}
+		valData = append(valData, strconv.FormatFloat(tiVal.Float64(), 'f', -1, 64))
+		tiYOY, err := yoy.Get(dt)
+		if err != nil {
+			yoyData = append(yoyData, "0")
+		} else {
+			yoyData = append(yoyData, strconv.FormatFloat(tiYOY.Float64(), 'f', -1, 64))
+		}
+		tiQOQ, err := qoq.Get(dt)
+		if err != nil {
+			qoqData = append(qoqData, "0")
+		} else {
+			qoqData = append(qoqData, strconv.FormatFloat(tiQOQ.Float64(), 'f', -1, 64))
+		}
+	}
+	tbl.Rows = [][]string{valData, yoyData, qoqData}
+	return tbl
+}
+
 // WriteJSON writes the data to a JSON file. To write a minimized JSON
 // file use an empty string for `prefix` and `indent`.
 func (ts *TimeSeries) WriteJSON(filename string, perm os.FileMode, prefix, indent string) error {
@@ -76,6 +131,6 @@ func (ts *TimeSeries) WriteJSON(filename string, perm os.FileMode, prefix, inden
 
 // WriteXLSX writes an XSLX file given a `TimeSeries`
 func (ts *TimeSeries) WriteXLSX(filename string, sheetName, dateColumnName, countColumnName string) error {
-	tbl := ts.ToTable(sheetName, dateColumnName, countColumnName, nil)
+	tbl := ts.Table(sheetName, dateColumnName, countColumnName, nil)
 	return table.WriteXLSX(filename, &tbl)
 }
