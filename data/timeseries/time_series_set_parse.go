341fbf038ba5fbb9cea764867cfc3d0a825e7853
grokify <johncwang@gmail.com>
2022-04-12 20:58:29 -0700

    feat: data/timeseries: add `ParseTableTimeSeriesSetMatrix()`

--- data/timeseries/time_series_set_parse.go
+++ data/timeseries/time_series_set_parse.go
@@ -8,10 +8,52 @@ import (
 	"github.com/grokify/gocharts/v2/data/table"
 )
 
-// ParseTableTimeSeriesSet create a `TimeSeriesSet` from a `table.Table` using the least
+// ParseTableTimeSeriesSetMatrix create a `TimeSeriesSet` from a `table.Table` using the least
+// amount of input to populate the data structure. The time must be in column 0 and the series
+// names must be in the column headers.
+func ParseTableTimeSeriesSetMatrix(tbl table.Table, isFloat bool, timeParseFunc func(s string) (time.Time, error)) (TimeSeriesSet, error) {
+	if timeParseFunc == nil {
+		timeParseFunc = ParseTimeFuncRFC3339
+	}
+	tss := NewTimeSeriesSet("")
+	tss.IsFloat = isFloat
+	for y, row := range tbl.Rows {
+		if len(row) <= 1 {
+			continue
+		}
+		dt, err := timeParseFunc(row[0])
+		if err != nil {
+			return tss, fmt.Errorf("cannot parse time [%s] in row [%d]", row[0], y)
+		}
+		for x := 1; x < len(row); x++ {
+			if x >= len(tbl.Columns) {
+				return tss, fmt.Errorf("no column header for column [%d] on row [%d]", x, y)
+			}
+			seriesName := tbl.Columns[x]
+			countString := row[x]
+			if isFloat {
+				countFloat, err := strconv.ParseFloat(countString, 64)
+				if err != nil {
+					return tss, fmt.Errorf("cannot parse count as float64 [%s] in row [%d]", row[x], y)
+				}
+				tss.AddFloat64(seriesName, dt, countFloat)
+			} else {
+				countInt, err := strconv.Atoi(countString)
+				if err != nil {
+					return tss, fmt.Errorf("cannot parse count as int [%s] in row [%d]", row[x], y)
+				}
+				tss.AddInt64(seriesName, dt, int64(countInt))
+			}
+		}
+	}
+	tss.Times = tss.TimeSlice(true)
+	return tss, nil
+}
+
+// ParseTableTimeSeriesSetFlat create a `TimeSeriesSet` from a `table.Table` using the least
 // amount of input to populate the data structure. It does not set the following
 // parameters which must be set manually: `Name`, `Interval`.
-func ParseTableTimeSeriesSet(tbl table.Table, timeColIdx, seriesNameColIdx, countColIdx uint, isFloat bool, timeParseFunc func(s string) (time.Time, error)) (TimeSeriesSet, error) {
+func ParseTableTimeSeriesSetFlat(tbl table.Table, timeColIdx, seriesNameColIdx, countColIdx uint, isFloat bool, timeParseFunc func(s string) (time.Time, error)) (TimeSeriesSet, error) {
 	if timeParseFunc == nil {
 		timeParseFunc = ParseTimeFuncRFC3339
 	}
@@ -56,4 +98,8 @@ func ParseTimeFuncMonthYear(s string) (time.Time, error) {
 
 func ParseTimeFuncRFC3339(s string) (time.Time, error) {
 	return time.Parse(time.RFC3339, s)
+}
+
+func ParseTimeFuncYearDotMonth(s string) (time.Time, error) {
+	return time.Parse("2006.01", s)
 }
