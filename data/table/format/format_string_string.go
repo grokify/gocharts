f9409e8d4a00dad05f60c3e8a6b948aceec1fdcc
grokify <johncwang@gmail.com>
2022-05-06 08:37:50 -0700

    cleanup: data/table/format: remove `FormatStringRemoveControls()` in favor of `ConvertRemoveControls()`

--- data/table/format/format_string_string.go
+++
@@ -1,9 +0,0 @@
-package format
-
-import "regexp"
-
-func FormatStringRemoveControls(s string) (string, error) {
-	// Tableau CSVs may have `\x00` control chars.
-	// use `Table.ForamatRows()` on all columns to remove.
-	return regexp.MustCompile(`\x00`).ReplaceAllString(s, ""), nil
-}
