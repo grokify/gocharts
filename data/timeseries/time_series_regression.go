a2d5dea7e3809ff16b38e6a19969fc3c63da214e
grokify <johncwang@gmail.com>
2022-05-13 03:15:17 -0700

    enhance: data/timeseries: update `TimeSeries.LinearRegression()` to support interval `timeutil.Month`

--- data/timeseries/time_series_regression.go
+++ data/timeseries/time_series_regression.go
@@ -3,33 +3,48 @@ package timeseries
 import (
 	"errors"
 
+	"github.com/grokify/mogo/time/month"
 	"github.com/grokify/mogo/time/timeutil"
 	"gonum.org/v1/gonum/stat"
 )
 
-// LinearRegressionYear only runs when interval=Year.
-func (ts *TimeSeries) LinearRegressionYear() (alpha, beta float64, err error) {
-	if ts.Interval != timeutil.Year {
-		err = errors.New("timeseries = year is required")
-	}
+var ErrIntervalNotSupported = errors.New("interval not supported")
 
+// LinearRegression returns the `alpha` and `beta` for the data series.
+// It currently only supports `month` and `year` time intervals. When
+// `month` is used, the time is converted to `MonthContinuous` from
+// `github.com/grokify/mogo/time/month`.
+func (ts *TimeSeries) LinearRegression() (alpha, beta float64, err error) {
 	xs, ys := []float64{}, []float64{}
-
-	for _, ti := range ts.ItemMap {
-		xs = append(xs, float64(ti.Time.Year()))
-		ys = append(ys, ti.Float64())
+	switch ts.Interval {
+	case timeutil.Year:
+		for _, ti := range ts.ItemMap {
+			xs = append(xs, float64(ti.Time.Year()))
+			ys = append(ys, ti.Float64())
+		}
+		alpha, beta = stat.LinearRegression(xs, ys, nil, false)
+	case timeutil.Month:
+		for _, ti := range ts.ItemMap {
+			xs = append(xs, float64(month.TimeToMonthContinuous(ti.Time)))
+			ys = append(ys, ti.Float64())
+		}
+		alpha, beta = stat.LinearRegression(xs, ys, nil, false)
+	default:
+		err = ErrIntervalNotSupported
 	}
-	alpha, beta = stat.LinearRegression(xs, ys, nil, false)
 	return
 }
 
 // LinearRegressionYearProjection only runs when interval=Year for now.
 // Use this to build tables.
 func (ts *TimeSeries) LinearRegressionYearProjection(years uint, constantYOY bool) error {
+	if ts.Interval != timeutil.Year {
+		return ErrIntervalNotSupported
+	}
 	if years == 0 {
 		return nil
 	}
-	alpha, beta, err := ts.LinearRegressionYear()
+	alpha, beta, err := ts.LinearRegression()
 	if err != nil {
 		return err
 	}
