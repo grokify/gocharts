4b3c1d5e2ca72f9d0301aa1a15c188f686619625
John Wang <johncwang@gmail.com>
2023-09-03 15:05:05 -0700

    feat: `data/table`: add `Table.RowsToMSS()`

--- data/table/row.go
+++ data/table/row.go
@@ -1,5 +1,9 @@
 package table
 
+import (
+	"errors"
+)
+
 // RowCellCounts returns a `map[int]int` where the key is the cell count
 // and the value is the number of rows.
 func (tbl *Table) RowCellCounts() map[int]int {
@@ -8,4 +12,19 @@ func (tbl *Table) RowCellCounts() map[int]int {
 		mii[len(row)] += 1
 	}
 	return mii
+}
+
+func (tbl *Table) RowsToMSS(keyIdx, valIdx uint) (map[string]string, error) {
+	m := map[string]string{}
+	kIdxInt := int(keyIdx)
+	vIdxInt := int(valIdx)
+	for _, r := range tbl.Rows {
+		if kIdxInt >= len(r) || vIdxInt >= len(r) {
+			return m, errors.New("index not present in row")
+		}
+		k := r[keyIdx]
+		v := r[valIdx]
+		m[k] = v
+	}
+	return m, nil
 }
