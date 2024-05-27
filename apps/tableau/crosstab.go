994864e9803ae889066826a84b7b47d349fb0a5d
grokify <johncwang@gmail.com>
2024-05-26 20:08:58 -0700

    feat: `apps/tableau`: add `ReadFileCrosstabXLSX()`, `WriteFileLineChartCrosstabXLSX()`

---
+++ apps/tableau/crosstab.go
@@ -0,0 +1,43 @@
+package tableau
+
+import (
+	"os"
+	"time"
+
+	"github.com/grokify/gocharts/v2/charts/google"
+	"github.com/grokify/gocharts/v2/data/table"
+	"github.com/grokify/gocharts/v2/data/timeseries"
+	"github.com/grokify/mogo/strconv/strconvutil"
+	"github.com/grokify/mogo/time/timeutil"
+)
+
+// ReadFileCrosstabXLSX reads a Tableau crosstab XLSX file where there is one sheet
+// named "Sheet 1" with Column headings, "empty" followed by "FullMonth Year", e.g.
+// "January 2024". The rows are various timeseries with numbers as integers with
+// commas for thousands.
+func ReadFileCrosstabXLSX(filename string, interval timeutil.Interval) (*timeseries.TimeSeriesSet, error) {
+	if tbl, err := table.ReadTableXLSXIndexFile(filename, 0, 1, true); err != nil {
+		return nil, err
+	} else if tss, err := timeseries.ParseTableTimeSeriesSetMatrixRows(*tbl, timeutil.IntervalMonth, false,
+		timeutil.ParseTimeCanonicalFunc("Jan 2006"),
+		strconvutil.AtoiMoreFunc(",", "."),
+		nil); err != nil {
+		return nil, err
+	} else {
+		tss.Interval = interval
+		return tss, nil
+	}
+}
+
+func WriteFileLineChartCrosstabXLSX(infile, outfile string, perm os.FileMode, interval timeutil.Interval, title string) error {
+	tss, err := ReadFileCrosstabXLSX(infile, interval)
+	if err != nil {
+		return err
+	}
+	lcm := google.NewLineChartMaterial()
+	lcm.Title = title
+	lcm.LoadTimeSeriesSetMonth(tss, func(t time.Time) string {
+		return t.Format("Jan 2006")
+	})
+	return lcm.WriteFilePage(outfile, perm)
+}
