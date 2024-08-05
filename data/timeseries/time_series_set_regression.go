100060c844af06383c46814a7fd829301d069da9
John Wang <johncwang@gmail.com>
2024-08-05 06:53:40 -0700

    style: `data/timeseries`: update `TimeSeriesSet.LinearRegressionYearProjection()`

--- data/timeseries/time_series_set_regression.go
+++ data/timeseries/time_series_set_regression.go
@@ -3,11 +3,11 @@ package timeseries
 // LinearRegressionYearProjection only runs when interval=Year.
 func (set *TimeSeriesSet) LinearRegressionYearProjection(years uint, constantYOY bool) error {
 	for name, ts := range set.Series {
-		err := ts.LinearRegressionYearProjection(years, constantYOY)
-		if err != nil {
+		if err := ts.LinearRegressionYearProjection(years, constantYOY); err != nil {
 			return err
+		} else {
+			set.Series[name] = ts
 		}
-		set.Series[name] = ts
 	}
 	return nil
 }
