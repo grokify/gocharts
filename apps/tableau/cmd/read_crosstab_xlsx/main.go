416ea3d40decb7489d10d14bf05587ed464307d0
John Wang <johncwang@gmail.com>
2024-08-17 22:13:41 -0700

    streamline: `apps/tableau`: simplify example

--- apps/tableau/cmd/read_crosstab_xlsx/main.go
+++ apps/tableau/cmd/read_crosstab_xlsx/main.go
@@ -9,26 +9,16 @@ import (
 )
 
 func main() {
-	if 1 == 0 {
-		filename := "path/to/monthly-items-trend.xlsx"
-		err := tableau.WriteFileLineChartCrosstabXLSX(filename, "chart_acounts.html", 0600, timeutil.IntervalMonth, "Monthly Accounts")
-		logutil.FatalErr(err)
+	filename := "path/to/monthly-items-trend.xlsx"
+	outfileHTML := "chart_apps.html"
+	outfilePNG := "chart_apps.png"
+	err := tableau.WriteFileLineChartCrosstabXLSX(filename, outfileHTML, 0600, timeutil.IntervalMonth, "Monthly Accounts")
+	logutil.FatalErr(err)
+	fmt.Printf("Wrote (%s)\n", outfileHTML)
 
-		fmt.Printf("Wrote (%s)\n", filename)
-	}
-
-	if 1 == 1 {
-		filename := "path/to/monthly-items-trend.xlsx"
-		outfileHTML := "chart_apps.html"
-		outfilePNG := "chart_apps.png"
-		err := tableau.WriteFileLineChartCrosstabXLSX(filename, outfileHTML, 0600, timeutil.IntervalMonth, "Monthly Onboarded Applications")
-		logutil.FatalErr(err)
-		fmt.Printf("Wrote (%s)\n", outfileHTML)
-
-		err = tableau.WriteFileLineChartWchartXLSX(filename, outfilePNG, timeutil.IntervalMonth)
-		logutil.FatalErr(err)
-		fmt.Printf("Wrote (%s)\n", outfilePNG)
-	}
+	err = tableau.WriteFileLineChartWchartXLSX(filename, outfilePNG, timeutil.IntervalMonth)
+	logutil.FatalErr(err)
+	fmt.Printf("Wrote (%s)\n", outfilePNG)
 
 	fmt.Println("DONE")
 }
