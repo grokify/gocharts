0bffd6712bdd661db20f99bfc4419e4216a5e1f7
John Wang <johncwang@gmail.com>
2024-08-25 05:17:23 -0700

    refactor: `charts/google/piechart`: refactor to use `google.Chart` interface

--- charts/google/piechart/piechart.go
+++ charts/google/piechart/piechart.go
@@ -1,13 +1,14 @@
 package piechart
 
 import (
-	"encoding/json"
 	"io"
 	"os"
 
 	"github.com/grokify/gocharts/v2/charts/google"
+	"github.com/grokify/gocharts/v2/data/histogram"
 	"github.com/grokify/gocharts/v2/data/piechart"
 	"github.com/grokify/gocharts/v2/data/table"
+	"github.com/grokify/mogo/encoding/jsonutil"
 )
 
 // Chart provides data for Google Material Pie Charts described here:
@@ -16,98 +17,108 @@ type Chart struct {
 	Title               string
 	Subtitle            string
 	ChartDiv            string
-	Width               int
-	Height              int
 	AddCountToName      bool
 	DefaultCategoryName string
 	DataTable           *google.DataTable
 	Columns             google.Columns
 	Data                piechart.PieChartData
-	GoogleOptions       Options
+	GoogleOptions       *Options
 }
 
-func NewPieChartMaterialInts(chartName, sliceName, sliceValueName string, vals map[string]int) Chart {
-	c := Chart{
+func NewPieChartMaterialInts(chartName, sliceName, sliceValueName string, vals map[string]int) *Chart {
+	chart := Chart{
 		Title: chartName,
 		Columns: google.Columns{
 			{Name: sliceName, Type: table.FormatString},
 			{Name: sliceValueName, Type: table.FormatInt},
 		},
 		Data: piechart.PieChartData{IsFloat: false},
-		GoogleOptions: Options{
+		GoogleOptions: &Options{
 			Title: chartName,
 		},
 	}
-	c.Data.AddInts(vals)
-	return c
+	chart.Data.AddInts(vals)
+	return &chart
 }
 
-func (cm *Chart) BuildDataTable() google.DataTable {
-	if cm.DataTable != nil {
-		return *cm.DataTable
+func (chart *Chart) LoadDataTableHistogram(h *histogram.Histogram, cols google.Columns) {
+	if len(cols) >= 0 {
+		chart.Columns = cols
 	}
-	colNamesAny := cm.Columns.NamesAny()
+	colNamesAny := chart.Columns.NamesAny()
 	if len(colNamesAny) < 2 {
 		colNamesAny = []any{"Categories", "Value"}
 	}
-	var matrix = [][]any{colNamesAny}
-	cm.Data.Sort()
-	for _, d := range cm.Data.Data {
+	var dt = google.DataTable{colNamesAny}
+	binNames := h.BinNamesMore(true, true, true)
+	for _, binName := range binNames {
+		binVal := h.GetOrDefault(binName, 0)
+		dt = append(dt, []any{binName, binVal})
+	}
+	chart.DataTable = &dt
+}
+
+func (chart *Chart) BuildDataTable() google.DataTable {
+	if chart.DataTable != nil {
+		return *chart.DataTable
+	}
+	colNamesAny := chart.Columns.NamesAny()
+	if len(colNamesAny) < 2 {
+		colNamesAny = []any{"Categories", "Value"}
+	}
+	var dt = google.DataTable{colNamesAny}
+	chart.Data.Sort()
+	for _, d := range chart.Data.Data {
 		var row []any
 		name := d.Name
-		if cm.AddCountToName {
-			name = d.NameWithCount(cm.DefaultCategoryName)
+		if chart.AddCountToName {
+			name = d.NameWithCount(chart.DefaultCategoryName)
 		}
 		if d.IsFloat {
 			row = append(row, name, d.ValFloat)
 		} else {
 			row = append(row, name, d.ValInt)
 		}
-		matrix = append(matrix, row)
+		dt = append(dt, row)
 	}
-	return matrix
+	return dt
 }
 
-func (cm *Chart) DataTableJSON() []byte {
-	matrix := cm.BuildDataTable()
-	return matrix.MustJSON()
+/*
+func (chart Chart) ChartDivOrDefault() string {
+	if div := strings.TrimSpace(chart.ChartDiv); div != "" {
+		return div
+	} else {
+		return google.DefaultChartDiv
+	}
+}
+*/
+
+func (chart *Chart) DataTableJSON() []byte {
+	dt := chart.BuildDataTable()
+	return dt.MustJSON()
 }
 
-func (cm *Chart) OptionsJSON() []byte {
-	return cm.GoogleOptions.MustJSON()
-}
-
-func (cm *Chart) ChartDivOrDefault() string {
-	if len(cm.ChartDiv) > 0 {
-		return cm.ChartDiv
+func (chart *Chart) ChartDivOrDefault() string {
+	if len(chart.ChartDiv) > 0 {
+		return chart.ChartDiv
 	}
 	return google.DefaultChartDiv
 }
 
-func (cm *Chart) HeightOrDefault() int {
-	if cm.Height > 0 {
-		return cm.Height
+func (chart *Chart) OptionsJSON() []byte {
+	if chart.GoogleOptions == nil {
+		return []byte(jsonutil.EmptyObject)
+	} else {
+		return chart.GoogleOptions.MustJSON()
 	}
-	return google.DefaultHeight
 }
+func (chart *Chart) PageHTML() string      { return PieChartMaterialPage(chart) }
+func (chart *Chart) PageTitle() string     { return chart.Title }
+func (chart *Chart) WritePage(w io.Writer) { WritePieChartMaterialPage(w, chart) }
 
-func (cm *Chart) WidthOrDefault() int {
-	if cm.Width > 0 {
-		return cm.Width
-	}
-	return google.DefaultWidth
-}
-
-func (cm *Chart) PageHTML() string {
-	return PieChartMaterialPage(*cm)
-}
-
-func (cm *Chart) WritePage(w io.Writer) {
-	WritePieChartMaterialPage(w, *cm)
-}
-
-func (cm *Chart) WriteFilePage(filename string, perm os.FileMode) error {
-	return os.WriteFile(filename, []byte(cm.PageHTML()), perm)
+func (chart *Chart) WriteFilePage(filename string, perm os.FileMode) error {
+	return os.WriteFile(filename, []byte(chart.PageHTML()), perm)
 }
 
 const (
@@ -127,15 +138,21 @@ type Options struct {
 	PieStartAngle     float64   `json:"pieStartAngle,omitempty"`
 }
 
+func (opts *Options) Inflate() {
+	if opts.Height == 0 {
+		opts.Height = google.DefaultHeight
+	}
+	if opts.Width == 0 {
+		opts.Width = google.DefaultWidth
+	}
+}
+
 // MustJSON represents the Google Charts JSON options map as defined here:
 // https://developers.google.com/chart/interactive/docs/gallery/piechart .
 // The output is intended to be used directly with the client-side JS library call.
-func (opts Options) MustJSON() []byte {
-	if b, err := json.Marshal(opts); err != nil {
-		return []byte("{}")
-	} else {
-		return b
-	}
+func (opts *Options) MustJSON() []byte {
+	opts.Inflate()
+	return jsonutil.MustMarshalOrDefault(opts, []byte(jsonutil.EmptyObject))
 }
 
 type TextStyle struct {
