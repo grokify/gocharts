6a63ab0b68ad4f26abdbfd5a1aa21dc27a124197
John Wang <johncwang@gmail.com>
2024-12-15 04:28:55 -0800

    refactor: `data/table`: add `ColumnDefinitions{}`; rename `ColumnDefinitionSet{}`

--- data/table/column_defs.go
+++ data/table/column_defs.go
@@ -8,15 +8,15 @@ import (
 
 // ColumnDefinitions provides a way to load definitions with their format types using
 // the `Table.LoadColumnDefinitions()` method.
-type ColumnDefinitions struct {
+type ColumnDefinitionSet struct {
 	DefaultFormat string
-	Definitions   []ColumnDefinition
+	Definitions   ColumnDefinitions
 }
 
-// BuildColumnDefinitions returns a `ColumnDefinitions{}` struct given a set of column
+// BuildColumnDefinitionSet returns a `ColumnDefinitions{}` struct given a set of column
 // names and a format map, similar to stored in `Table{}`.`
-func BuildColumnDefinitions(names []string, formatMap map[int]string) ColumnDefinitions {
-	cds := ColumnDefinitions{}
+func BuildColumnDefinitionSet(names []string, formatMap map[int]string) ColumnDefinitionSet {
+	cds := ColumnDefinitionSet{}
 	if f, ok := formatMap[-1]; ok {
 		cds.DefaultFormat = f
 	}
@@ -32,19 +32,37 @@ func BuildColumnDefinitions(names []string, formatMap map[int]string) ColumnDefi
 	return cds
 }
 
+type ColumnDefinitions []ColumnDefinition
+
+func (cd ColumnDefinitions) SourceNames(defToName, errOnMissing bool) ([]string, error) {
+	var out []string
+	for i, cdi := range cd {
+		if cdi.SourceName != "" {
+			out = append(out, cdi.SourceName)
+		} else if defToName {
+			out = append(out, cdi.Name)
+		} else if errOnMissing {
+			return out, fmt.Errorf("empty at index %d", i)
+		} else {
+			out = append(out, "")
+		}
+	}
+	return out, nil
+}
+
 // ColumnDefinition represents one column including its name and format.
 type ColumnDefinition struct {
-	Name   string
-	Format string
+	Name         string // internal name
+	Format       string
+	ExportName   string // for exports only
+	SourceName   string // used for imports
+	DefaultValue string
 }
 
 // LoadColumnDefinitions loads a set of column definitions with names and formats. It
 // can be used to add columns to a table without any existing columns or to add to a
 // table with existing columns.
-func (tbl *Table) LoadColumnDefinitions(colDefs *ColumnDefinitions) {
-	if colDefs == nil {
-		return
-	}
+func (tbl *Table) LoadColumnDefinitionSet(colDefs ColumnDefinitionSet) {
 	if tbl.Columns == nil {
 		tbl.Columns = Columns{}
 	}
@@ -70,6 +88,6 @@ func (tbl *Table) LoadColumnDefinitions(colDefs *ColumnDefinitions) {
 }
 
 // ColumnDefinitions returns a `ColumnDefinitions{}` struct for the `Table{}`.
-func (tbl *Table) ColumnDefinitions() ColumnDefinitions {
-	return BuildColumnDefinitions(slices.Clone(tbl.Columns), tbl.FormatMap)
+func (tbl *Table) ColumnDefinitionSet() ColumnDefinitionSet {
+	return BuildColumnDefinitionSet(slices.Clone(tbl.Columns), tbl.FormatMap)
 }
