52bee7eb183490820e34dc7d64c16af651eaf47a
John Wang <johncwang@gmail.com>
2025-03-22 06:33:39 -0700

    feat: `data/table/tabulator`: add constants

---
+++ data/table/tabulator/constants.go
@@ -0,0 +1,10 @@
+// tabulator provides helper methods for rendering HTML
+// with Tabulator (http://tabulator.info/)
+package tabulator
+
+const (
+	URLCSSLatest = "https://unpkg.com/tabulator-tables/dist/css/tabulator.min.css"
+	URLJSLatest  = "https://unpkg.com/tabulator-tables/dist/js/tabulator.min.js"
+	HTMLURLs     = `<link href="https://unpkg.com/tabulator-tables/dist/css/tabulator.min.css" rel="stylesheet">
+<script type="text/javascript" src="https://unpkg.com/tabulator-tables/dist/js/tabulator.min.js"></script>`
+)
