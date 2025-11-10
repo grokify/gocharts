5e27b3213aee9703015b1c7d7b55bc1588eeefa9
John Wang <johncwang@gmail.com>
2025-11-10 00:44:37 -0800

    feat: `data/table`: ad `FormatPercent`

--- data/table/constants.go
+++ data/table/constants.go
@@ -1,11 +1,13 @@
 package table
 
 const (
-	FormatFloat  = "float"
-	FormatInt    = "int"
-	FormatString = "string"
-	FormatDate   = "date"
-	FormatTime   = "time"
-	FormatURL    = "url"
-	StyleSimple  = "border:1px solid #000;border-collapse:collapse"
+	FormatDate    = "date"
+	FormatFloat   = "float"
+	FormatInt     = "int"
+	FormatPercent = "percent"
+	FormatString  = "string"
+	FormatTime    = "time"
+	FormatURL     = "url"
+
+	StyleSimple = "border:1px solid #000;border-collapse:collapse"
 )
